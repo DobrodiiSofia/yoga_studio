66113018d598452f534b8ad82493a177958ae018
DobrodiiSofia <sofija291007@gmail.com>
2025-04-08 00:19:09 +0300

    Adapted for two pages was done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
-let burger = document.getElementById("burger");
-let menu = document.querySelector(".menu");
 
-
-burger.addEventListener("click", () => {
-    menu.classList.toggle("active"); 
-});
